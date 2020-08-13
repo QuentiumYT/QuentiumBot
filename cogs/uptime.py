81128ead8d0727909768e3d790b6af79313e0648
QuentiumYT <quentin.lienhardt@gmail.com>
2020-08-13 11:43:15 +0200

    Added uptime command
    - Translations for uptime

---
+++ cogs/uptime.py
@@ -0,0 +1,42 @@
+import discord
+from discord.ext import commands
+from datetime import datetime
+from QuentiumBot import GetData, get_translations, start_time
+
+# Basic command configs
+cmd_name = "uptime"
+tran = get_translations()
+aliases = [] if not tran[cmd_name]["fr"]["aliases"] else tran[cmd_name]["fr"]["aliases"].split("/")
+
+class UptimeInfos(commands.Cog):
+    """Uptime command in Statistics section"""
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command(
+        name=cmd_name,
+        aliases=aliases,
+        pass_context=True,
+        no_pm=True
+    )
+    @commands.guild_only()
+    async def uptime_cmd(self, ctx):
+        # Get specific server data
+        if isinstance(ctx.channel, discord.TextChannel):
+            data = await GetData.retrieve_data(self, ctx.message.guild)
+            lang_server = data[0]
+        else:
+            lang_server = "en"
+        cmd_tran = tran[cmd_name][lang_server]
+
+        # Doesn't respond to bots
+        if not ctx.message.author.bot == True:
+            time = (datetime.now() - start_time).total_seconds()
+            m, s = divmod(int(time), 60)
+            h, m = divmod(m, 60)
+            d, h = divmod(h, 24)
+            await ctx.send(cmd_tran["msg_uptime"].format(d, h, m, s))
+
+def setup(client):
+    client.add_cog(UptimeInfos(client))
