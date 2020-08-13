fc25d533e77f8a1cbd8c9d0e2446ddb38ce2012c
QuentiumYT <quentin.lienhardt@gmail.com>
2020-08-13 12:28:07 +0200

    Added uptime command
    - Translations for ping
    - Changed time method to perf_counter

---
+++ cogs/ping.py
@@ -0,0 +1,44 @@
+import discord, time
+from discord.ext import commands
+from QuentiumBot import GetData, get_translations, start_time
+
+# Basic command configs
+cmd_name = "ping"
+tran = get_translations()
+aliases = [] if not tran[cmd_name]["fr"]["aliases"] else tran[cmd_name]["fr"]["aliases"].split("/")
+
+class PingInfos(commands.Cog):
+    """Ping command in Statistics section"""
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command(
+        name=cmd_name,
+        aliases=aliases,
+        pass_context=True,
+        no_pm=True
+    )
+    @commands.guild_only()
+    async def ping_cmd(self, ctx):
+        # Get specific server data
+        if isinstance(ctx.channel, discord.TextChannel):
+            data = await GetData.retrieve_data(self, ctx.message.guild)
+            lang_server = data[0]
+        else:
+            lang_server = "en"
+        cmd_tran = tran[cmd_name][lang_server]
+
+        # Doesn't respond to bots
+        if not ctx.message.author.bot == True:
+            before = time.perf_counter()
+            await ctx.trigger_typing()
+            ping_bot = round((time.perf_counter() - before) * 1000)
+            before = time.perf_counter()
+            tmp = await ctx.send(cmd_tran["msg_ping_test"])
+            ping_msg = round((time.perf_counter() - before) * 1000)
+            await tmp.delete()
+            await ctx.send(cmd_tran["msg_pong"].format(ping_bot, ping_msg))
+
+def setup(client):
+    client.add_cog(PingInfos(client))
