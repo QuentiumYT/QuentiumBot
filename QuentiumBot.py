32a8c32bb063f1d443f61d1622c14ec90603fafe
QuentiumYT <quentin.lienhardt@gmail.com>
2020-09-03 16:08:59 +0200

    Added back all listeners and functions
    - Added new functions
    - New main comments on functions
    - Updated requirements
    - Added scripts (external programs)

--- QuentiumBot.py
+++ QuentiumBot.py
@@ -1,15 +1,21 @@
-import discord, json, os
+import discord, asyncio, json, os, requests, psutil
 from discord.ext import commands
-from datetime import datetime
+from datetime import datetime, date, timedelta
+from ftplib import FTP
+from subprocess import check_output
 
-# Defines if the bot is under debug or not
+__version__ = 1.0
+__author__ = "QuentiumYT"
+__filename__ = "QuentiumBot"
+
 debug = True
-# Folder containing all cogs
+windows = os.name == "nt"
+
 cogs_folder = "cogs."
 startup_cogs = [f.replace(".py", "") for f in os.listdir(cogs_folder) if os.path.isfile(os.path.join(cogs_folder, f))]
 start_time = datetime.now()
 
-# TYPE Data class
+# TYPE Data classes and functions
 
 def get_config(section, key):
     """Get the configuration for tokens and credentials"""
@@ -43,10 +49,12 @@ def match_id(dis_id):
     else:
         return False
 
-def is_owner(ctx):
+def is_owner(ctx, user_id=False):
     """Check if the user ID is one of the owner acccount"""
 
     # Quentium's user IDs
+    if user_id:
+        return any(x == user_id for x in client.owner_ids)
     return any(x == ctx.message.author.id for x in client.owner_ids)
 
 class HandleData:
@@ -165,7 +173,18 @@ client = commands.Bot(command_prefix=get_prefix,
                       case_insensitive=True,
                       max_messages=999999)
 
-# TYPE Global events
+async def post_topgg_data():
+    top_gg_url = "https://top.gg/api/bots/{" + str(client.user.id) + "}/stats"
+    headers = {"Authorization": get_config("GLOBAL", "token_dbl")}
+    payload = {"server_count": len(client.guilds)}
+    try:
+        requests.post(top_gg_url,
+                      data=payload,
+                      headers=headers)
+    except:
+        pass
+
+# TYPE Global bot events
 
 @client.event
 async def on_ready():
@@ -178,15 +197,22 @@ async def on_ready():
     print("Logged in as %s#%s" % (client.user.name, client.user.discriminator))
     print("ID: " + str(client.user.id))
     print("\nStarting at: " + start_time.strftime("%d.%m.%Y - %H:%M:%S"))
+    if debug:
+        presence = "Oh, a QuentiumBot rewrite!?"
+    else:
+        presence = "+help | bot.quentium.fr"
+        await post_topgg_data()
     await client.change_presence(
         status=discord.Status.online,
         activity=discord.Activity(
-            name="Oh, a QuentiumBot rewrite!?",
+            name=presence,
             type=discord.ActivityType.playing)
     )
 
 @client.listen()
 async def on_message(message):
+    """Bot on message listener"""
+
     if isinstance(message.channel, discord.TextChannel):
         data = await HandleData.retrieve_data(client, message.guild)
         server_id = message.guild.id
@@ -201,16 +227,100 @@ async def on_message(message):
     if client.user.mention == message.content.replace("!", ""):
         await message.channel.send(tran["bot_prefix"].format(prefix_server, prefix_server))
 
-    triggers = await HandleData.get_data(client, "triggers")
     if not message.author.bot == True:
+        triggers = await HandleData.get_data(client, "triggers")
         if server_id and any(x == str(server_id) for x in triggers.keys()):
             if any(x == message.content.lower() for x in triggers[str(server_id)].keys()):
                 response = triggers[str(server_id)].get(message.content.lower())
-                return await message.channel.send(response)
+                await message.channel.send(response)
+
+    ### Delete advertising messages on TheSweMaster server
+    if server_id == 199189022894063627:  # TheSweMaster server ID
+        if len([x.name for x in message.author.roles]) == 1:
+            if any(x in message.content.lower() for x in ["oauth2", "discord.gg"]):
+                await message.delete()
+
+@client.event
+async def on_member_join(member):
+    """Bot member join a server listener"""
+
+    server_id = member.guild.id
+    autorole_server = await HandleData.retrieve_data(client, member.guild)[2]
+
+    if autorole_server:
+        role = discord.utils.get(member.guild.roles, id=autorole_server)
+        if role:
+            try:
+                await member.add_roles(role)
+            except:
+                pass
+
+    ### Welcome message and ban ads accounts on TheSweMaster server
+    if server_id == 199189022894063627: # TheSweMaster server ID
+        if any(x in member.name for x in ["discord.gg", "twitter.com"]):
+            await member.ban()
+            msg = "A bot has been banned because we don't like them :ok_hand:"
+            return await discord.utils.get(member.guild.channels, id=290905147826110464).send(msg)
+        else:
+            msg = f"Hey {member.mention} ! Welcome on ***{member.guild.name}***! Feel free to ask for a cookie :cookie:"
+            return await discord.utils.get(member.guild.channels, id=199189022894063627).send(msg)
+
+    ### Welcome message on QuentiumBot support server
+    elif server_id == 380373195473027074: # Support QB server ID
+        embed = discord.Embed(color=0x14F5F5)
+        embed.title = "Welcome " + member.name
+        embed.url = "https://bot.quentium.fr"
+        embed.description = "You now have joined the Testers of QuentiumBot yikes! <a:yikes:731504778894508072> Spend some good time with us!"
+        embed.set_thumbnail(url=member.avatar_url)
+        embed.set_footer(text="By QuentiumBot")
+        return await discord.utils.get(member.guild.channels, id=380373687284793344).send(embed=embed)
+
+async def on_server_join(server):
+    """Bot added to a new server listener"""
+
+    if not debug:
+        await post_topgg_data()
+
+async def on_server_remove(server):
+    """Bot removed from a server listener"""
+
+    if not debug:
+        await post_topgg_data()
+
+@client.event
+async def on_raw_reaction_add(ctx):
+    """Non-cache reaction addition listener"""
+
+    if is_owner(ctx, ctx.user_id):
+        user = client.get_user(ctx.user_id)
+        server = client.get_guild(ctx.guild_id)
+        channel = server.get_channel(ctx.channel_id)
+        message = await channel.fetch_message(ctx.message_id)
+        if message.embeds and "Choisissez un ordinateur à démarrer" in message.embeds[0].title:
+            await message.remove_reaction(ctx.emoji, user)
+            emo = ctx.emoji.name
+            if emo and emo[:2] == "pc":
+                if emo == "pc1":
+                    args = "etherwake -i eth0 40:16:7E:AD:F7:21"
+                    tmp = await channel.send(str(ctx.emoji) + " Démarrage de **PC Quentium**")
+                elif emo == "pc2":
+                    args = "etherwake -i eth0 "
+                    tmp = await channel.send(str(ctx.emoji) + " Démarrage de **PC portable Quentium**")
+                elif emo == "pc3":
+                    args = "etherwake -i eth0 40:61:86:93:B7:C7"
+                    tmp = await channel.send(str(ctx.emoji) + " Démarrage de **PC Bureau**")
+                elif emo == "pc4":
+                    args = "etherwake -i eth0 40:16:7E:AD:7B:6C"
+                    tmp = await channel.send(str(ctx.emoji) + " Démarrage de **PC Space**")
+                await exec_command(args, message)
+                await asyncio.sleep(10)
+                return await tmp.delete()
 
 if not debug:
     @client.event
     async def on_command_error(ctx, error):
+        """Bot command error handler"""
+
         if isinstance(ctx.channel, discord.TextChannel):
             data = await HandleData.retrieve_data(client, ctx.message.guild)
             lang_server = data[0]
@@ -255,6 +365,182 @@ if not debug:
         file.write(" --- ".join(map(str, infos)) + "\n")
         file.close()
 
+# TYPE Global functions
+
+async def do_tasks():
+    """Execute cron tasks actions"""
+
+    ### TimeToEat project menu upload
+    if datetime.today().weekday() == 6:
+        if windows:
+            await exec_command("python scripts/menu4tte.py", None)
+        else:
+            await exec_command("python3 scripts/menu4tte.py", None)
+    # TimeToEat project data generation
+    # if datetime.today().weekday() < 5:
+    #     if windows:
+    #         await exec_command("python scripts/data4tte.py 130", None)
+    #     else:
+    #         await exec_command("python3 scripts/data4tte.py 130", None)
+
+    ### Insoumis server kick inactive members
+    serv = client.get_guild(391272643229384705) # Insoumis server ID
+    channel = serv.get_channel(485168827580284948) # Insoumis logs channel ID
+    kick_list = [x for x in [x for x in serv.members if not x.bot] if len(x.roles) <= 1]
+    kick_list_name = [x.name for x in [x for x in serv.members if not x.bot] if len(x.roles) <= 1]
+    for member in kick_list:
+        await member.kick()
+    if kick_list_name:
+        content = "- " + "\n- ".join(kick_list_name)
+    else:
+        content = "Personne :smile:"
+    embed = discord.Embed(color=0xFF1111)
+    embed.title = f"Membres expulsés : {len(kick_list_name)}"
+    embed.description = content
+    embed.set_footer(text=str(datetime.now().strftime("%d.%m.%Y - %H:%M:%S")))
+    await channel.send(embed=embed)
+
+async def push_bot_stats():
+    """Send a JSON recap with data to the website"""
+
+    data = await HandleData.get_data(client, "data")
+
+    stats = {}
+    stats["hosted"] = "VPS Oserya.fr"
+    stats["owner"] = "QuentiumYT#0207"
+    stats["helper"] = "Microsoft Visual Studio#1943"
+    stats["linux"] = "Debian GNU/Linux 10 (buster)"
+    time = round((datetime.now() - start_time).total_seconds())
+    m, s = divmod(int(time), 60)
+    h, m = divmod(m, 60)
+    d, h = divmod(h, 24)
+    stats["uptime"] = f"{d} Days, {h} Hours, {m} Minutes, {s} Seconds"
+    stats["creation_date"] = "16/08/2017 | 17h46"
+    creation_date = date(2017, 8, 16)
+    today_date = date.today()
+    stats["creation_days"] = (today_date - creation_date).days
+    stats["lines_count"] = sum(1 for line in open("QuentiumBot.py"))
+    stats["memory"] = int(psutil.virtual_memory().used >> 20)
+    stats["storage"] = int((os.stat("QuentiumBot.py").st_size >> 10) + (os.stat("Quentium67Bot.py").st_size >> 10))
+    bot_commands_get_total = 0
+    for serv in data.keys():
+        bot_commands_get_total += data[serv]["commands_server"]
+    stats["cmd_total"] = int(bot_commands_get_total)
+    users = 0
+    for serv in client.guilds:
+        users += len(serv.members)
+    stats["users_total"] = int(users)
+    stats["emojis_total"] = len(client.emojis)
+    stats["servers_total"] = len(client.guilds)
+    bot_lang_fr = bot_lang_en = bot_lang_de = 0
+    for serv in data.keys():
+        if "fr" in data[serv]["lang_server"]:
+            bot_lang_fr += 1
+        elif "en" in data[serv]["lang_server"]:
+            bot_lang_en += 1
+        elif "de" in data[serv]["lang_server"]:
+            bot_lang_de += 1
+    stats["registered_fr"] = int(bot_lang_fr)
+    stats["registered_en"] = int(bot_lang_en)
+    stats["registered_de"] = int(bot_lang_de)
+    with open("data/botstats.json", "w", encoding="utf-8", errors="ignore") as file:
+        json.dump(stats, file, indent=4)
+    ftp = FTP(get_config("GLOBAL", "ftp_host"),
+              get_config("GLOBAL", "ftp_login"),
+              get_config("GLOBAL", "ftp_passwd"))
+    file = open("data/botstats.json", "rb")
+    ftp.storbinary("STOR /bot/json/botstats.json", file)
+    file.close()
+    ftp.close()
+
+async def loop_repeat():
+    """Loop running for cron task at 7AM"""
+
+    await client.wait_until_ready()
+    now = datetime.today().replace(microsecond=0)
+    num_days_month = (date(now.year + now.month // 12, now.month % 12 + 1, 1) - timedelta(days=1)).day
+    clock = now.replace(day=now.day, hour=7, minute=0, second=0, microsecond=0)
+    if now.hour > clock.hour:
+        if int(now.strftime("%d")) == num_days_month:
+            clock = now.replace(month=now.month + 1, day=1, hour=7, minute=0, second=0, microsecond=0)
+        else:
+            clock = now.replace(day=now.day + 1, hour=7, minute=0, second=0, microsecond=0)
+    elif now.hour == clock.hour:
+        clock = now.replace(day=now.day + 1, hour=7, minute=0, second=0, microsecond=0)
+    while not client.is_closed():
+        time_now = datetime.today().replace(microsecond=0)
+        timer_finished = time_now
+        sec_time = int(time_now.strftime("%S"))
+        min_time = int(time_now.strftime("%M"))
+        hour_time = int(time_now.strftime("%H"))
+        day_time = int(time_now.strftime("%d"))
+        if sec_time > 55 and sec_time <= 59:
+            if min_time == 59:
+                if hour_time == 23:
+                    if day_time == num_days_month:
+                        timer_finished = time_now.replace(month=time_now.month + 1, day=1, hour=0, minute=0, second=0)
+                    else:
+                        timer_finished = time_now.replace(day=time_now.day + 1, hour=0, minute=0, second=0)
+                else:
+                    timer_finished = time_now.replace(hour=time_now.hour + 1, minute=0, second=0)
+            else:
+                timer_finished = time_now.replace(minute=time_now.minute + 1, second=0)
+        if timer_finished == clock:
+            await do_tasks()
+            await push_bot_stats()
+            now = datetime.today().replace(microsecond=0)
+            if day_time == num_days_month:
+                if now.month == 12:
+                    clock = now.replace(year=now.year + 1, month=1, day=1, hour=7, minute=0, second=0, microsecond=0)
+                else:
+                    clock = now.replace(month=now.month + 1, day=1, hour=7, minute=0, second=0, microsecond=0)
+            else:
+                clock = now.replace(day=now.day + 1, hour=7, minute=0, second=0, microsecond=0)
+        await asyncio.sleep(5)
+
+loop = asyncio.get_event_loop()
+try:
+    client.loop.create_task(loop_repeat())
+except:
+    loop.run_forever()
+
+async def exec_command(args, msg):
+    """Execute a command on the hosting server"""
+
+    def emo(text):
+        return discord.utils.get(client.emojis, name=text)
+
+    if any(args == x for x in ["runpc", "setco"]):
+        embed = discord.Embed(color=0x000000)
+        if args == "runpc":
+            emojis = list(map(emo, ["pc1", "pc2", "pc3", "pc4"]))
+            content = f"{emojis[0]} Quentium PC\n{emojis[1]} Quentium Laptop\n{emojis[2]} Office PC\n{emojis[3]} Space PC"
+            embed.title = f"{emo('vote')} Choisissez un ordinateur à démarrer :"
+        # else:
+        #     emojis = list(map(emo, ["co1", "co2", "co3", "co4"]))
+        #     content = f"{emojis[0]} ON 19H\n{emojis[1]} ON 22H\n{emojis[2]} OFF 19H\n{emojis[3]} OFF 22H"
+        #     embed.title = f"{emo('vote')} Choisissez une action à réaliser pour la connexion :"
+        embed.description = content
+        msg = await msg.channel.send(embed=embed)
+        for emoji in emojis:
+            await msg.add_reaction(emoji)
+        return
+
+    try:
+        result = check_output(args,
+                              shell=True,
+                              stderr=-2)
+    except Exception as e:
+        result = type(e).__name__ + ": " + str(e)
+
+    try:
+        await msg.channel.send("```autohotkey\n{}\n```".format(result.decode("cp1252")))
+    except:
+        try:
+            await msg.channel.send("```autohotkey\n{}\n```".format(result.decode("ISO-8859-1")))
+        except:
+            await msg.channel.send("```autohotkey\n{}\n```".format(str(result)))
+
 # TYPE Global commands
 
 @client.command(hidden=True)
@@ -294,6 +580,7 @@ async def reload(ctx, extension):
 # TYPE Start
 
 if __name__ == "__main__":
+    # Load extensions
     for extension in startup_cogs:
         try:
             client.load_extension(cogs_folder + extension)
