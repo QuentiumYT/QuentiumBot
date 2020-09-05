fe586a781872fd697d918ca086366ba079af7c50
QuentiumYT <quentin.lienhardt@gmail.com>
2020-09-06 00:40:03 +0200

    Lang refactor
    - Added comments
    - Removed old no_pm argument
    - Removed return in else
    - Renamed command argument

--- cogs/lang.py
+++ cogs/lang.py
@@ -16,11 +16,10 @@ class LangAdminConfig(commands.Cog):
     @commands.command(
         name=cmd_name,
         aliases=aliases,
-        pass_context=True,
-        no_pm=True
+        pass_context=True
     )
     @commands.guild_only()
-    async def lang_cmd(self, ctx, *, args=None):
+    async def lang_cmd(self, ctx, *, lang=None):
         # Get specific server data
         if isinstance(ctx.channel, discord.TextChannel):
             data = await HandleData.retrieve_data(self, ctx.message.guild)
@@ -31,20 +30,23 @@ class LangAdminConfig(commands.Cog):
 
         # Doesn't respond to bots
         if not ctx.message.author.bot == True:
+            # Check user perms or owner
             if not(ctx.message.author.guild_permissions.administrator or is_owner(ctx)):
                 return await ctx.send(cmd_tran["msg_perm_admin_user"].format(ctx.message.author.name))
-            if not args:
+            # No args given
+            if not lang:
                 return await ctx.send(cmd_tran["msg_specify_lang"])
-            else:
-                if any(args == x for x in ["fr", "en"]):
-                    if lang_server == args:
-                        await ctx.send(cmd_tran["msg_lang_same"])
-                    else:
-                        await HandleData.change_lang(self, ctx, args)
-                        # Lang changed before sending the msg
-                        await ctx.send(tran[cmd_name][args]["msg_lang_set"])
+
+            # Check if lang is existing
+            if any(lang == x for x in ["fr", "en"]):
+                if lang_server == lang:
+                    await ctx.send(cmd_tran["msg_lang_same"])
                 else:
-                    return await ctx.send(cmd_tran["msg_lang_not_exist"])
+                    await HandleData.change_lang(self, ctx, lang)
+                    # Lang changed before sending the msg
+                    await ctx.send(tran[cmd_name][lang]["msg_lang_set"])
+            else:
+                await ctx.send(cmd_tran["msg_lang_not_exist"])
 
 def setup(client):
     client.add_cog(LangAdminConfig(client))
