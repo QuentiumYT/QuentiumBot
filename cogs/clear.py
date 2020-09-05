a5e9addf2f8899fb1370cdf085784e9cadb254c1
QuentiumYT <quentin.lienhardt@gmail.com>
2020-09-05 23:06:26 +0200

    Clear refactor
    - Added comments
    - Removed old no_pm argument
    - Renamed parsed arg
    - Removed return in else

--- cogs/clear.py
+++ cogs/clear.py
@@ -16,11 +16,10 @@ class ClearAdminRights(commands.Cog):
     @commands.command(
         name=cmd_name,
         aliases=aliases,
-        pass_context=True,
-        no_pm=True
+        pass_context=True
     )
     @commands.guild_only()
-    async def clear_cmd(self, ctx, *, args=None):
+    async def clear_cmd(self, ctx, *, number=None):
         # Get specific server data
         if isinstance(ctx.channel, discord.TextChannel):
             data = await HandleData.retrieve_data(self, ctx.message.guild)
@@ -31,22 +30,28 @@ class ClearAdminRights(commands.Cog):
 
         # Doesn't respond to bots
         if not ctx.message.author.bot == True:
+            # Check user perms
             if not ctx.message.author.permissions_in(ctx.message.channel).manage_messages:
                 return await ctx.send(cmd_tran["msg_perm_manage_msg_user"].format(ctx.message.author.name))
+            # Check bot perms
             if not ctx.message.channel.guild.me.guild_permissions.manage_messages:
                 return await ctx.send(cmd_tran["msg_perm_manage_msg_bot"])
-            if not args:
-                await ctx.send(cmd_tran["msg_specify_number"])
-            else:
-                if args.split()[0].isdecimal():
-                    number = int(args.split()[0])
-                    if number < 99 and number > 0:
-                        limit = number + 1
-                        await ctx.message.channel.purge(limit=limit)
-                    else:
-                        return await ctx.send(cmd_tran["msg_limit_number"])
+            # No number given
+            if not number:
+                return await ctx.send(cmd_tran["msg_specify_number"])
+
+            # Get the first number given and check if it's decimal
+            if number.split()[0].isdecimal():
+                number = int(number.split()[0])
+                # Number between 1 and 99 if errors
+                if number < 99 and number > 0:
+                    limit = number + 1
+                    # Clear the messages
+                    await ctx.message.channel.purge(limit=limit)
                 else:
-                    await ctx.send(cmd_tran["msg_invalid_number"])
+                    await ctx.send(cmd_tran["msg_limit_number"])
+            else:
+                await ctx.send(cmd_tran["msg_invalid_number"])
 
 def setup(client):
     client.add_cog(ClearAdminRights(client))
