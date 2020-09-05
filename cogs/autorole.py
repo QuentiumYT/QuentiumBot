24b8d2deb4df722e5f868267f66752e5bae1108a
QuentiumYT <quentin.lienhardt@gmail.com>
2020-09-05 22:49:38 +0200

    Autorole refactor
    - Added comments
    - Removed old no_pm argument
    - Missing autorole_server variable
    - Dedent code after return
    - Remove last return
    - Changed elif to if after return

--- cogs/autorole.py
+++ cogs/autorole.py
@@ -16,8 +16,7 @@ class AutoroleAdminConfig(commands.Cog):
     @commands.command(
         name=cmd_name,
         aliases=aliases,
-        pass_context=True,
-        no_pm=True
+        pass_context=True
     )
     @commands.guild_only()
     async def autorole_cmd(self, ctx, *, args=None):
@@ -29,42 +28,51 @@ class AutoroleAdminConfig(commands.Cog):
             prefix_server = data[3]
         else:
             lang_server = "en"
+            autorole_server = None
             prefix_server = "+"
         cmd_tran = tran[cmd_name][lang_server]
 
         # Doesn't respond to bots
         if not ctx.message.author.bot == True:
+            # Check if perms or owner
             if not(ctx.message.author.guild_permissions.manage_roles or is_owner(ctx)):
                 return await ctx.send(cmd_tran["msg_perm_roles_user"].format(ctx.message.author.name))
+            # Check if bot has perms
             if not ctx.message.channel.guild.me.guild_permissions.manage_roles:
                 return await ctx.send(cmd_tran["msg_perm_roles_bot"])
+            # No args
             if not args:
                 return await ctx.send(cmd_tran["msg_invalid_arg"].format(prefix_server))
-            else:
-                if any(x == args.lower() for x in ["remove", "delete"]):
-                    await HandleData.change_autorole(self, ctx, None)
-                    return await ctx.send(cmd_tran["msg_role_deleted"])
-                elif any(x == args.lower() for x in ["show", "see"]):
-                    if autorole_server == None:
-                        return await ctx.send(cmd_tran["msg_role_not_defined"])
-                    else:
-                        role = discord.utils.get(ctx.message.guild.roles, id=autorole_server)
-                        if role == None:
-                            return await ctx.send(cmd_tran["msg_unknown_role"])
-                        else:
-                            return await ctx.send(cmd_tran["msg_current_role"].format(role.name))
-                elif match_id(args):
-                    role = discord.utils.get(ctx.message.guild.roles, id=match_id(args))
-                else:
-                    for role in ctx.message.guild.roles:
-                        if args.lower() == role.name.lower():
-                            role = discord.utils.get(ctx.message.guild.roles, name=role.name)
-                            break
-                    else:
-                        return await ctx.send(cmd_tran["msg_invalid_role"])
 
-                await HandleData.change_autorole(self, ctx, role.id)
-                return await ctx.send(cmd_tran["msg_role_set"].format(role.name))
+            # Remove the autorole
+            if any(x == args.lower() for x in ["remove", "delete"]):
+                await HandleData.change_autorole(self, ctx, None)
+                return await ctx.send(cmd_tran["msg_role_deleted"])
+            # See the autorole
+            if any(x == args.lower() for x in ["show", "see"]):
+                if autorole_server == None:
+                    return await ctx.send(cmd_tran["msg_role_not_defined"])
+                # Get the role object
+                role = discord.utils.get(ctx.message.guild.roles, id=autorole_server)
+                if role == None:
+                    return await ctx.send(cmd_tran["msg_unknown_role"])
+                return await ctx.send(cmd_tran["msg_current_role"].format(role.name))
+
+            # If the role is any kind of ID
+            if match_id(args):
+                role = discord.utils.get(ctx.message.guild.roles, id=match_id(args))
+            # Find the role using it's name
+            else:
+                for role in ctx.message.guild.roles:
+                    if args.lower() == role.name.lower():
+                        role = discord.utils.get(ctx.message.guild.roles, name=role.name)
+                        break
+                else:
+                    return await ctx.send(cmd_tran["msg_invalid_role"])
+
+            # Modify the role in the config
+            await HandleData.change_autorole(self, ctx, role.id)
+            await ctx.send(cmd_tran["msg_role_set"].format(role.name))
 
 def setup(client):
     client.add_cog(AutoroleAdminConfig(client))
