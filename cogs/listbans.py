61f81a654c8cba07c4f6671acea4b4e539c58aa3
QuentiumYT <quentin.lienhardt@gmail.com>
2020-09-06 14:43:43 +0200

    Listbans refactor
    - Added comments
    - Removed old no_pm argument

--- cogs/listbans.py
+++ cogs/listbans.py
@@ -16,8 +16,7 @@ class ListbansInfos(commands.Cog):
     @commands.command(
         name=cmd_name,
         aliases=aliases,
-        pass_context=True,
-        no_pm=True
+        pass_context=True
     )
     @commands.guild_only()
     async def listbans_cmd(self, ctx):
@@ -31,11 +30,14 @@ class ListbansInfos(commands.Cog):
 
         # Doesn't respond to bots
         if not ctx.message.author.bot == True:
+            # Get the member ban list
             list_banned = await ctx.guild.bans()
             if list_banned:
+                # List all the bans
                 content = "\n- ".join([x[1].name for x in list_banned])
             else:
                 content = cmd_tran["msg_any"]
+            # Send an embed
             embed = discord.Embed(color=0x664211)
             embed.title = cmd_tran["msg_title"]
             embed.description = "- " + content
