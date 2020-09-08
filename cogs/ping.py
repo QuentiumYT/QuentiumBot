971085e25850c88f1e0f0a503b67389b2df4255d
QuentiumYT <quentin.lienhardt@gmail.com>
2020-09-08 23:05:00 +0200

    Ping refactor
    - Added comments
    - Removed old no_pm argument

--- cogs/ping.py
+++ cogs/ping.py
@@ -16,10 +16,8 @@ class PingInfos(commands.Cog):
     @commands.command(
         name=cmd_name,
         aliases=aliases,
-        pass_context=True,
-        no_pm=True
+        pass_context=True
     )
-    @commands.guild_only()
     async def ping_cmd(self, ctx):
         # Get specific server data
         if isinstance(ctx.channel, discord.TextChannel):
@@ -31,13 +29,17 @@ class PingInfos(commands.Cog):
 
         # Doesn't respond to bots
         if not ctx.message.author.bot == True:
+            # Ping the time to trigger text typing
             before = time.perf_counter()
             await ctx.trigger_typing()
             ping_bot = round((time.perf_counter() - before) * 1000)
+            # Ping the sending message latency
             before = time.perf_counter()
             tmp = await ctx.send(cmd_tran["msg_ping_test"])
             ping_msg = round((time.perf_counter() - before) * 1000)
+            # Delete the test message
             await tmp.delete()
+            # Send both pings
             await ctx.send(cmd_tran["msg_pong"].format(ping_bot, ping_msg))
 
 def setup(client):
