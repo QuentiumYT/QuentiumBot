fb4499cf1eb79e8bd7a0ddd32d30a60f84002e1f
QuentiumYT <quentin.lienhardt@gmail.com>
2020-09-09 20:56:53 +0200

    Fixed first menu recuperation, changed lib to convert to jpg
    - Using https for second menu

--- scripts/menu4tte.py
+++ scripts/menu4tte.py
@@ -1,7 +1,6 @@
-import os
+import os, fitz
 from urllib.request import urlopen, urlretrieve
 from datetime import datetime
-from pdf2image import convert_from_path
 from ftplib import FTP
 
 from _tte import ftp_tte
@@ -13,16 +12,17 @@ ftp = FTP(ftp_tte["host"],
 # Broken for now (last working 28/06/2020) changed menu link
 try:
     # Get menu from the website and download
-    url_static = "http://www.lyc-heinrich-haguenau.ac-strasbourg.fr/docman-documents/menus-de-la-restauration-scolaire/"
+    url_static = "https://www.lycee-heinrich-nessel.fr/vivre-au-lycee/hebergement-restauration/menus-de-la-restauration-scolaire/"
     content_page = urlopen(url_static).read()
     data_url_menu = str(content_page).split("restauration-scolaire/")[-1].split(" data-title=")[0]
     url_menu = url_static + data_url_menu
     urlretrieve(url_menu, "menu.pdf")
 
     # Convert the menu to jpg format
-    pages = convert_from_path("menu.pdf", 500)
-    for page in pages:
-        page.save("menu.jpg", "JPEG")
+    doc = fitz.open("menu.pdf")
+    pic = doc.loadPage(0).getPixmap(matrix=fitz.Matrix(150 / 72, 150 / 72))
+    pic.writePNG("menu.jpg")
+    doc.close()
 
     # Connect to FTP TimeToEat and send menu file
     f = open("menu.jpg", "rb")
@@ -39,12 +39,12 @@ try:
     try:
         # Get menu from the website and download
         week = str(datetime.utcnow().isocalendar()[1] + 0).zfill(2)
-        url_menu = f"http://www.macantineetmoi.com/sites/default/files/etablissement/sainte-philo/sainte-philo_S{week}.jpg"
+        url_menu = f"https://www.macantineetmoi.com/sites/default/files/etablissement/sainte-philo/sainte-philo_S{week}.jpg"
         urlretrieve(url_menu, "menu1.jpg")
     except:
         # Get menu next week from the website and download
         week = str(datetime.utcnow().isocalendar()[1] + 2).zfill(2)
-        url_menu = f"http://www.macantineetmoi.com/sites/default/files/etablissement/sainte-philo/sainte-philo_S{week}.jpg"
+        url_menu = f"https://www.macantineetmoi.com/sites/default/files/etablissement/sainte-philo/sainte-philo_S{week}.jpg"
         urlretrieve(url_menu, "menu1.jpg")
 
     # Connect to FTP TimeToEat and send menu file
