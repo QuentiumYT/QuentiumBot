3ac33225323b0d3e4e31fe0fa9ba96a019b9113e
QuentiumYT <quentin.lienhardt@gmail.com>
2020-10-03 13:06:01 +0200

    Added new Schuman PDF menu

--- scripts/menu4tte.py
+++ scripts/menu4tte.py
@@ -1,15 +1,14 @@
 import os, fitz
-from urllib.request import urlopen, urlretrieve
-from datetime import datetime
+from urllib.request import urlopen, urlretrieve, install_opener, build_opener
+from datetime import datetime, timedelta
 from ftplib import FTP
 
-from _tte import ftp_tte
+from _tte import ftp_tte, schuman_auth
 
 ftp = FTP(ftp_tte["host"],
           ftp_tte["user"],
           ftp_tte["pwd"])
 
-# Broken for now (last working 28/06/2020) changed menu link
 try:
     # Get menu from the website and download
     url_static = "https://www.lycee-heinrich-nessel.fr/vivre-au-lycee/hebergement-restauration/menus-de-la-restauration-scolaire/"
@@ -54,6 +53,31 @@ try:
 
     # Remove files
     os.remove("menu1.jpg")
+except Exception as e:
+    print(e)
+
+try:
+    # Add 1 day to get next week
+    menu_date = (datetime.now() + timedelta(days=1)).strftime("%Y-%m-%d")
+    url_menu = f"https://infoconso-schuman.salamandre.tm.fr/Prod/API/public/v1/Pdf/1/1/3/{menu_date}/PDF"
+    opener = build_opener()
+    opener.addheaders = [("Authorization", schuman_auth)]
+    install_opener(opener)
+    urlretrieve(url_menu, "menu2.pdf")
+
+    # Convert the menu to jpg format
+    doc = fitz.open("menu2.pdf")
+    pic = doc.loadPage(0).getPixmap(matrix=fitz.Matrix(150 / 72, 150 / 72))
+    pic.writePNG("menu2.jpg")
+    doc.close()
+
+    # Connect to FTP TimeToEat and send menu file
+    f = open("menu2.jpg", "rb")
+    ftp.storbinary("STOR img/menu2.jpg", f)
+    f.close()
+
+    os.remove("menu2.pdf")
+    os.remove("menu2.jpg")
 except Exception as e:
     print(e)
 
