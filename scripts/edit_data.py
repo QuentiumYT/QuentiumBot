289b3b2e9d523ec833e30f0ce6d3c1628ece0090
QuentiumYT <quentin.lienhardt@gmail.com>
2022-11-09 22:05:45 +0100

    Purge servers with less than 10 commands

--- scripts/edit_data.py
+++ scripts/edit_data.py
@@ -4,12 +4,13 @@ import json
 with open("data.json", "r", encoding="utf-8") as file:
     content = json.load(file)
 
-# Convert autorole_server data to int
-for srv in content.keys():
-    autorole = content[srv]["autorole_server"]
-    if autorole:
-        content[srv]["autorole_server"] = int(autorole)
+new_content = content.copy()
+
+# Remove key if commands_server < 10
+for srv, data in content.items():
+    if data["commands_server"] < 10:
+        del new_content[srv]
 
 # Dump the content  to the global data file
 with open("data.json", "w", encoding="utf-8") as file:
-    json.dump(content, file, indent=4)
+    json.dump(new_content, file, indent=4)
