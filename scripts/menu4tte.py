763f5366658886f4a3d9b05e01e200478c45dd6c
QuentiumYT <quentin.lienhardt@gmail.com>
2022-11-09 22:26:56 +0100

    Rewrite menu fetch cleaner way

--- scripts/menu4tte.py
+++ scripts/menu4tte.py
@@ -1,6 +1,7 @@
 import os, fitz
 from urllib.request import urlopen, urlretrieve, install_opener, build_opener
-from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta, timezone
 from ftplib import FTP
 
 from _tte import ftp_tte, schuman_auth
@@ -15,52 +16,50 @@ ftp.login(ftp_tte["user"],
 
 try:
     # Get menu from the website and download
-    url_static = "https://www.lycee-heinrich-nessel.fr/vivre-au-lycee/hebergement-restauration/menus-de-la-restauration-scolaire/"
+    url_static = "https://www.lycee-heinrich-nessel.fr/index.php/vivre-au-lycee/hebergement-et-restauration"
     content_page = urlopen(url_static).read()
-    data_url_menu = str(content_page).split("restauration-scolaire/")[-1].split(" data-title=")[0]
-    url_menu = url_static + data_url_menu
-    urlretrieve(url_menu, "menu.pdf")
+    html = BeautifulSoup(content_page, "html.parser")
+    url_menu = html.find("a", class_="uk-position-cover")["href"].replace(" ", "%20")
+    urlretrieve(url_menu, "menu_heinrich_nessel.pdf")
 
     # Convert the menu to jpg format
-    doc = fitz.open("menu.pdf")
+    doc = fitz.open("menu_heinrich_nessel.pdf")
     pic = doc.load_page(0).get_pixmap(matrix=fitz.Matrix(150 / 72, 150 / 72))
-    pic.save("menu.jpg")
+    pic.save("menu_heinrich_nessel.jpg")
     doc.close()
 
     # Connect to FTP TimeToEat and send menu file
-    f = open("menu.jpg", "rb")
-    ftp.storbinary("STOR menu.jpg", f)
-    f.close()
+    with open("menu_heinrich_nessel.jpg", "rb") as file:
+        ftp.storbinary("STOR menu_heinrich_nessel.jpg", file)
 
     # Remove files
     if remove:
-        os.remove("menu.pdf")
-        os.remove("menu.jpg")
+        os.remove("menu_heinrich_nessel.pdf")
+        os.remove("menu_heinrich_nessel.jpg")
 except Exception as e:
-    print(e)
+    print("Heinrich Nessel:", e)
 
 try:
     try:
         # Get menu from the website and download
-        week = str(datetime.utcnow().isocalendar()[1] + 1).zfill(2)
+        week = str(datetime.now(timezone.utc).isocalendar()[1] + 1).zfill(2)
         url_menu = f"https://www.macantineetmoi.com/sites/default/files/etablissement/sainte-philo/sainte-philo_S{week}.jpg"
-        urlretrieve(url_menu, "menu1.jpg")
-    except:
+        urlretrieve(url_menu, "menu_sainte_philomene.jpg")
+    except Exception:
         # Get menu next week from the website and download
-        week = str(datetime.utcnow().isocalendar()[1] + 0).zfill(2)
+        week = str(datetime.now(timezone.utc).isocalendar()[1] + 0).zfill(2)
         url_menu = f"https://www.macantineetmoi.com/sites/default/files/etablissement/sainte-philo/sainte-philo_S{week}.jpg"
-        urlretrieve(url_menu, "menu1.jpg")
+        urlretrieve(url_menu, "menu_sainte_philomene.jpg")
 
     # Connect to FTP TimeToEat and send menu file
-    f = open("menu1.jpg", "rb")
-    ftp.storbinary("STOR menu1.jpg", f)
-    f.close()
+    with open("menu_sainte_philomene.jpg", "rb") as file:
+        ftp.storbinary("STOR menu_sainte_philomene.jpg", file)
 
     # Remove files
     if remove:
-        os.remove("menu1.jpg")
+        os.remove("menu_sainte_philomene.jpg")
 except Exception as e:
-    print(e)
+    print("Sainte Philomène:", e)
 
 try:
     # Add 1 day to get next week
@@ -69,25 +68,24 @@ try:
     opener = build_opener()
     opener.addheaders = [("Authorization", schuman_auth)]
     install_opener(opener)
-    urlretrieve(url_menu, "menu2.pdf")
+    urlretrieve(url_menu, "menu_robert_schuman.pdf")
 
     # Convert the menu to jpg format
-    doc = fitz.open("menu2.pdf")
+    doc = fitz.open("menu_robert_schuman.pdf")
     pic = doc.load_page(0).get_pixmap(matrix=fitz.Matrix(150 / 72, 150 / 72))
-    pic.save("menu2.jpg")
+    pic.save("menu_robert_schuman.jpg")
     doc.close()
 
     # Connect to FTP TimeToEat and send menu file
-    f = open("menu2.jpg", "rb")
-    ftp.storbinary("STOR menu2.jpg", f)
-    f.close()
+    with open("menu_robert_schuman.jpg", "rb") as file:
+        ftp.storbinary("STOR menu_robert_schuman.jpg", file)
 
     # Remove files
     if remove:
-        os.remove("menu2.pdf")
-        os.remove("menu2.jpg")
+        os.remove("menu_robert_schuman.pdf")
+        os.remove("menu_robert_schuman.jpg")
 except Exception as e:
-    print(e)
+    print("Robert Schuman:", e)
 
 ftp.close()
 
